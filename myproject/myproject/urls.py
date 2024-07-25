0d7776cdbfa7d41a1ffa3d52872e5ae4fd6fa18e
aneeshvishwanaath <aneeshvishwanaath@gmail.com>
2024-07-25 16:25:02 +0530

    Update urls.py
    Registration/Login JWT Tokens

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for myproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
@@ -24,9 +8,7 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from users.serializers import UserSerializer, RegisterSerializer
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+
 
 schema_view = get_schema_view(
     openapi.Info(
